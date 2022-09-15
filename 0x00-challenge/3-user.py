60685b87a0ea46af30bfe1be6e4c484333b9667b
doloadama <doloadama@outlook.com>
2022-09-15 14:24:03 +0000

    .

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -37,8 +37,8 @@ class User():
         - `None` if `pwd` is not a string
         - Hash `pwd` in MD5 before assign to `__password`
         """
-        if pwd is None or type(pwd) is str:
-            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
+        if pwd is not None and type(pwd) is str:
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
@@ -52,6 +52,7 @@ class User():
             return False
         if self.__password is None:
             return False
+
         return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
 
 
